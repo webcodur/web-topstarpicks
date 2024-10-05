42fb20445ab695cf4277bbd8ff0b7afee8db07ca
webcodur <webcodur@gmail.com>
2024-10-06 01:37:11 +0900

    port 조정 및 cross-env 설치

--- app_server/src/server.js
+++ app_server/src/server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
 	cors({
-		origin: 'http://localhost:3000',
+		origin: 'http://localhost:3002',
 		methods: ['GET', 'POST', 'PUT', 'DELETE'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
 	})
