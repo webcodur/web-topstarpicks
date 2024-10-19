0f877905fabc8800030208f25b3cc4ee30f70ed3
webcodur <webcodur@gmail.com>
2024-10-19 13:28:34 +0900

    컨텐츠에서 데이터 속성 없는 경우 처리

--- app_client/src/view/contents/RecommendationCard.jsx
+++ app_client/src/view/contents/RecommendationCard.jsx
@@ -1,5 +1,4 @@
 import React, { forwardRef } from 'react';
-import { Link } from 'react-router-dom';
 import {
 	StyledCard,
 	StyledCardContent,
@@ -14,64 +13,84 @@ import {
 } from './ContentsStyle';
 
 const RecommendationCard = forwardRef(
-	({ recommendation, index, personInfo }, ref) => (
-		<StyledCard ref={ref}>
-			<StyledCardContent>
-				<StyledTitle>
-					NO {index + 1}: &nbsp; {recommendation.title}
-				</StyledTitle>
-
-				<div>
-					<p style={{ fontSize: '17px' }}>
-						{recommendation.creator || '작가 미상'}
-						{' ('}
-						{recommendation.release_date
-							? new Date(recommendation.release_date).toLocaleDateString()
-							: '작성일 미상'}
-						{')'}
-					</p>
+	({ recommendation, index, personInfo }, ref) => {
+		// 개행 문자를 <br /> 태그로 변환하는 함수
+		const formatText = (text) => {
+			return text.split('\n').map((line, i) => (
+				<React.Fragment key={i}>
+					{line}
 					<br />
-				</div>
+				</React.Fragment>
+			));
+		};
 
-				<ImageContainer>
-					<StyledBookImage
-						src={
-							recommendation.img_link ||
-							`https://via.placeholder.com/150?text=${encodeURIComponent(
-								recommendation.title
-							)}`
-						}
-						alt={recommendation.title}
-					/>
-				</ImageContainer>
+		return (
+			<StyledCard ref={ref}>
+				<StyledCardContent>
+					<StyledTitle>
+						NO {index + 1}: &nbsp; {recommendation.title}
+					</StyledTitle>
 
-				{recommendation.reason.length > 1 && (
-					<QuoteContainer>
-						<b>추천사 / 독서경위</b>:
-						<QuoteText>{recommendation.reason}</QuoteText>
-						<SourceLink to={recommendation.recommendation_source}>
-							{' '}
-							(소스)
-						</SourceLink>
-					</QuoteContainer>
-				)}
+					<div>
+						<p style={{ fontSize: '17px' }}>
+							{recommendation.creator || '작가 미상'}
+							{' ('}
+							{recommendation.release_date
+								? new Date(recommendation.release_date).toLocaleDateString()
+								: '작성일 미상'}
+							{')'}
+						</p>
+						<br />
+					</div>
 
-				<MediaDescContainer>
-					<b>내용</b>: {recommendation.mediaDescription}
-				</MediaDescContainer>
-				<br />
+					{recommendation.img_link.length > 0 && (
+						<ImageContainer>
+							<StyledBookImage
+								src={
+									recommendation.img_link ||
+									`https://via.placeholder.com/150?text=${encodeURIComponent(
+										recommendation.title
+									)}`
+								}
+								alt={recommendation.title}
+							/>
+						</ImageContainer>
+					)}
 
-				{recommendation.affiliate_link && (
-					<AffiliateLink
-						to={recommendation.affiliate_link}
-						target="_blank"
-						rel="noopener noreferrer">
-						yes24 링크
-					</AffiliateLink>
-				)}
-			</StyledCardContent>
-		</StyledCard>
-	)
+					{recommendation.reason.length > 1 && (
+						<QuoteContainer>
+							<div style={{ textAlign: 'center', margin: '10px' }}>
+								<b>추천사 / 콘텐츠 수용 경로</b>
+							</div>
+							<QuoteText>{formatText(recommendation.reason)}</QuoteText>
+							<SourceLink to={recommendation.recommendation_source}>
+								{' '}
+								(소스)
+							</SourceLink>
+						</QuoteContainer>
+					)}
+
+					{recommendation.mediaDescription.length > 0 && (
+						<MediaDescContainer>
+							<div style={{ textAlign: 'center', margin: '10px' }}>
+								<b>내용</b>
+							</div>
+							{formatText(recommendation.mediaDescription)}
+						</MediaDescContainer>
+					)}
+
+					{recommendation.affiliate_link && (
+						<AffiliateLink
+							to={recommendation.affiliate_link}
+							target="_blank"
+							rel="noopener noreferrer">
+							yes24 링크
+						</AffiliateLink>
+					)}
+				</StyledCardContent>
+			</StyledCard>
+		);
+	}
 );
 
 export default RecommendationCard;
